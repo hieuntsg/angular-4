93ba5437b475bb3a8d14b5fad37543b56f290d1e
Hieu Nguyen <evenhieu@gmail.com>
2017-12-24 22:44:58 +0700

    Add new loading component

---
+++ src/app/components/loading-data/loading-data.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-data',
+  templateUrl: './loading-data.component.html',
+  styleUrls: ['./loading-data.component.scss']
+})
+export class LoadingDataComponent implements OnInit {
+
+  @Input() loading: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
